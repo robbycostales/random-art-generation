4ae58af9edcf091f57c4bba2984c9742efacdf85
Zoe Offermann <zoffermann15>
2017-02-15 01:30:35 -0500

    edited some typos

--- Zoe_art_module.py
+++ Zoe_art_module.py
@@ -4,7 +4,7 @@ from random import randint
 
 wn = turtle.Screen()
 wn.colormode(255)
-im = Image.open("cool_colors.png")
+im = Image.open("noice.png")
 im_size = im.size
 (xrange,yrange) = im_size
 
@@ -19,7 +19,7 @@ def array_of_rgb(im, xrange, yrange):
     return rgb
 
 
-def parc_down_array(array):
+def parse_down_array(array):
     rgb_list = []
     for i in array:
         for rgb in i:
@@ -27,20 +27,22 @@ def parc_down_array(array):
                 rgb_list.append(rgb)
 
     return rgb_list
-rgb_list = parc_down_array(array_of_rgb(im, xrange, yrange))
+rgb_list = parse_down_array(array_of_rgb(im, xrange, yrange))
 
 
 
 
 class Art:
-    def __init__(self,color_pallet,pic_size_x,pic_size_y):
+    def __init__(self, color_pallet, pic_size_x, pic_size_y):
         self.t = turtle.Turtle()
         self.color_list = color_pallet
         self.xbound = pic_size_x
         self.ybound = pic_size_y
-        self.mode = int(input("enter 1 for rectangular mode and 0 for circular mode.(in circle mdoe the pic_size_x is the radius y does not matter):   "))
+        self.mode = int(input
+        ("enter 1 for rectangular mode and 0 for circular mode."""
+            "(in circle mode the pic_size_x is the radius y does not matter):   "))
 
-    def draw_square(self,color,startx,starty,size):
+    def draw_square(self, color, startx, starty, size):
         self.t.speed(0)
         self.t.color(color)
         self.t.fillcolor(color)
@@ -53,7 +55,7 @@ class Art:
             self.t.right(90)
         self.t.end_fill()
 
-    def draw_rectangle(self,color,startx,starty,size1,size2):
+    def draw_rectangle(self, color, startx, starty, size1, size2):
         self.t.speed(0)
         self.t.color(color)
         self.t.fillcolor(color)
@@ -108,9 +110,8 @@ class Art:
         self.t.color((255,255,255))
         self.t.circle(self.xbound+100)
 
-
     def draw_art(self):
-        wn.tracer(100,0)
+        wn.tracer(1000,0)
         for i in range(500):
             num = randint(0,3)
             size1 = randint(10,225)
@@ -122,9 +123,9 @@ class Art:
             if num == 0:
                 self.draw_rhombus(self.color_list[color], x_bound, y_bound, size1, size2, angle)
             if num == 1:
-                self.draw_rectangle(self.color_list[color],x_bound,y_bound,size1,size2)
+                self.draw_rectangle(self.color_list[color], x_bound, y_bound, size1, size2)
             if num == 2:
-                self.draw_square(self.color_list[color],x_bound,y_bound,size2)
+                self.draw_square(self.color_list[color], x_bound, y_bound,size2)
         if self.mode == 0:
             self.clean_perimeter_circle()
         else:
