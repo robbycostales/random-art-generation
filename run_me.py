f0d410c6e4406d511e93ae020baaf696aa310383
Zoe Offermann <zoffermann15>
2017-02-21 14:58:23 -0500

    added a run_me file that runs the modules. Edited robby's code so that it ran on my computer.

---
+++ run_me.py
@@ -0,0 +1,28 @@
+from max_bill import MaxBill
+import turtle
+from Zoe_art_module import run_art_generation
+
+
+wn = turtle.Screen()
+
+artist = int(input("which artist do you want to display? 1 for Max Bill, 2 for Bryce Hudson, and 3 for __:  "))
+
+if artist == 1:
+    for _ in range(10):
+        for i in (wn.turtles()): # this is here becasue there was a random visible turtle that I could not find.
+            if i.isvisible() is True:
+                i.ht()
+        a = MaxBill()
+        a.create_painting()
+
+
+elif artist == 2:
+
+    run_art_generation()
+    wn.mainloop()
+
+elif artist == 3:
+    print("naol's")
+
+else:
+    print("Incorrect input, try again!")
